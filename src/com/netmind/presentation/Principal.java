80143233b284c7daf614d880a2c07790840f6d82
jordiascension <jordiascension@enterpriseconsulting.es>
2021-02-16 21:06:23 +0100

    Initial commit, with calculator structure.

---
+++ src/com/netmind/presentation/Principal.java
@@ -0,0 +1,13 @@
+package com.netmind.presentation;
+
+import com.netmind.business.Calculator;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first java project");
+	}
+
+}
