1828e32543bda87464f0cb33da05cf27d787c79e
jordiascension <jordiascension@enterpriseconsulting.es>
2021-02-23 19:57:54 +0100

    Created a console screen with new student closes #2

--- src/com/netmind/presentation/Principal.java
+++ src/com/netmind/presentation/Principal.java
@@ -1,20 +1,10 @@
 package com.netmind.presentation;
 
-import java.util.Scanner;
-
-import com.netmind.business.StudentBl;
-import com.netmind.model.Student;
-
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Scanner scanner = new Scanner(System.in);
-		Student student = new Student();
-		StudentBl studentBl = new StudentBl();
-
-		System.out.println("¿Qué opción quiere seleccionar?");
+		StudentConsole.selectOperation();
 	}
 
 }
---
+++ src/com/netmind/presentation/StudentConsole.java
@@ -0,0 +1,59 @@
+package com.netmind.presentation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import com.netmind.business.StudentBl;
+import com.netmind.model.Student;
+
+public class StudentConsole {
+
+	public static void selectOperation() {
+		Scanner scanner = new Scanner(System.in);
+		StudentBl studentBl = new StudentBl();
+
+		int option;
+
+		do {
+			showPrincipalMenu();
+			// The nextInt() method does not deal with the EOL token,
+			// while nextLine() does.
+			option = Integer.parseInt(scanner.nextLine());
+
+			switch (option) {
+			case 1:
+				Student student = new Student();
+				addNewStudent(student, scanner);
+				studentBl.add(student);
+				break;
+			}
+		} while (option != 3);
+		scanner.close();
+	}
+
+	private static void showPrincipalMenu() {
+		System.out.println("¿Qué opción quiere seleccionar?");
+		System.out.println("1.Agregar un nuevo estudiante");
+		System.out.println("2.Calcular el estudiante con mayor edad");
+		System.out.println("3.Salir del programa");
+	}
+
+	private static void addNewStudent(Student student, Scanner scanner) {
+		System.out.println("1.Agregar un nuevo estudiante");
+		System.out.println("Introduce nombre: ");
+		student.setName(scanner.nextLine());
+
+		System.out.println("Introduce apellidos: ");
+		student.setSurname(scanner.nextLine());
+
+		System.out.println("Introduce Fecha de nacimiento: ");
+		try {
+			student.setDateOfBirth(new SimpleDateFormat("dd/MM/yyyy")
+					.parse(scanner.nextLine()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
