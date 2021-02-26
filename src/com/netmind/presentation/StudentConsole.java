39525cd0b395a584ef121135f67668311d6f98f9
jordiascension <jordiascension@enterpriseconsulting.es>
2021-02-26 02:16:28 +0100

    Student Console implemented with enum and StudentDaoIntegrationTest structure

--- src/com/netmind/presentation/StudentConsole.java
+++ src/com/netmind/presentation/StudentConsole.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import com.netmind.business.StudentBl;
+import com.netmind.model.EnumStudent;
 import com.netmind.model.Student;
 
 public class StudentConsole {
@@ -12,7 +13,7 @@ public class StudentConsole {
 	public static void selectOperation() {
 		Scanner scanner = new Scanner(System.in);
 		StudentBl studentBl = new StudentBl();
-
+		EnumStudent enumStudent = null;
 		int option;
 
 		do {
@@ -20,27 +21,33 @@ public class StudentConsole {
 			// The nextInt() method does not deal with the EOL token,
 			// while nextLine() does.
 			option = Integer.parseInt(scanner.nextLine());
+			enumStudent = EnumStudent.fromValue(option);
 
-			switch (option) {
-			case 1:
+			switch (enumStudent) {
+			case ADD_STUDENT:
 				Student student = new Student();
 				addNewStudent(student, scanner);
 				studentBl.add(student);
 				break;
+			case EXIT:
+				System.out.println("Good Bye!!");
+				break;
+			default:
+				break;
 			}
-		} while (option != 3);
+		} while (option != enumStudent.EXIT.value());
 		scanner.close();
 	}
 
 	private static void showPrincipalMenu() {
 		System.out.println("¿Qué opción quiere seleccionar?");
-		System.out.println("1.Agregar un nuevo estudiante");
-		System.out.println("2.Calcular el estudiante con mayor edad");
-		System.out.println("3.Salir del programa");
+		System.out.println("0.Agregar un nuevo estudiante");
+		System.out.println("1.Calcular el estudiante con mayor edad");
+		System.out.println("2.Salir del programa");
 	}
 
 	private static void addNewStudent(Student student, Scanner scanner) {
-		System.out.println("1.Agregar un nuevo estudiante");
+		System.out.println("0.Agregar un nuevo estudiante");
 		System.out.println("Introduce nombre: ");
 		student.setName(scanner.nextLine());
 
